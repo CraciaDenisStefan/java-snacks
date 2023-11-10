94a3b5bf988f2fbd99dcbb992f9a3132a9563769
CraciDenisStefan <crdenis13@gmail.com>
2023-11-10 11:24:16 +0100

    Snack1 ok

--- src/org/lesson/java/Snack1.java
+++ src/org/lesson/java/Snack1.java
@@ -1,5 +1,23 @@
 package org.lesson.java;
 
-public class Snack1 {
+import java.util.Scanner;
 
+public class Snack1 {
+	
+	public static void main(String[] args) {
+		Scanner in =  new Scanner(System.in);
+		
+		System.out.print("Inserisci un numero:");
+		String numero = in.nextLine();
+		int n =  Integer.valueOf(numero);
+		
+		in.close();
+		
+		if (n % 2 == 0) {
+			System.out.println("Il numero e pari:" + n);
+		}else {
+			int nSuccessivo = n+1;
+			System.out.println("Il numero non e pari:" + nSuccessivo );
+		}
+	}
 }
